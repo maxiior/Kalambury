90484d292c8ef63e14fb87bec2aaa57023921808
mikolajadamski <mikolaj.adamski47@gmail.com>
2021-04-16 13:38:29 +0200

    update: MessagesList.js
    pass setMessage function to MessagesList, if length of messages is greater than 5, only 5 latest elements are displayed

--- React/src/components/MessagesList.js
+++ React/src/components/MessagesList.js
@@ -1,6 +1,10 @@
 import './styles/messageslist.css';
 
-const MessagesList = ({messages}) => {
+const MessagesList = ({messages, setMessage}) => {
+    if(messages.length >5){
+        setMessage(messages.slice(1,6))
+    }
+    console.log(messages)
     return (
         <div>
             {messages.slice(0).reverse().map((m) => (
