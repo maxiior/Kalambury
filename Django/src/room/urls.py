818862bb1c22453b92df0a9be2c0c0a68026d41a
Dominik AD <dominik716@wp.pl>
2021-04-19 22:39:30 +0200

    Added room creation

--- Django/src/room/urls.py
+++ Django/src/room/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import index, room
+from .views import create_room
 
 app_name = 'room'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('create/', create_room, name="create"),
     path('<str:room_code>/', room, name='room'),
 ]
--- Django/src/room/views.py
+++ Django/src/room/views.py
@@ -1,4 +1,11 @@
+from os import read
+
 from django.shortcuts import render
+from rest_framework.decorators import api_view, renderer_classes
+from rest_framework.renderers import StaticHTMLRenderer, JSONRenderer
+from rest_framework.response import Response
+import json, uuid
+from datetime import datetime
 
 
 def index(request):
@@ -11,3 +18,17 @@ def room(request, room_code):
         'room_code': room_code,
         'username': username,
     })
+
+
+@api_view(['GET'])
+@renderer_classes([JSONRenderer])
+def create_room(request):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    response = {
+        "status": "success",
+        "id": f"{uuid.uuid4()}".replace("-", ""),
+        "created_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    }
+    return Response(response)
