4fdc52ba0a1482900b3f56bf517cfff48ebaa201
dominik7680 <46323893+dominik7680@users.noreply.github.com>
2021-04-20 17:11:00 +0200

    Update Footer.js

--- React/src/components/Footer.js
+++ React/src/components/Footer.js
@@ -3,7 +3,7 @@ import './styles/footer.css';
 const Footer = () => {
     return (
         <footer>
-            Developed by Dominik Adamek (Senior Backend Developer), Maksim Brzezinski (CFO), Izabela Błażejewicz (Creative Director), Mikołaj Adamski (CEO)
+            Developed by Dominik Adamek (Senior System Architect), Maksim Brzezinski (CFO), Izabela Błażejewicz (Creative Director), Mikołaj Adamski (CEO)
         </footer>
     )
 }
